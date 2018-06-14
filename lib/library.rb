6e2942811169e0f67a0175b08a797d5d3cea5762
Danil Davydov <Linadakill@gmail.com>
2018-06-14 17:02:25 +0300

    almost done with save and load lib

--- lib/library.rb
+++ lib/library.rb
@@ -12,6 +12,8 @@ module Library
   class BookLibrary
     attr_reader :authors, :books, :readers, :orders
 
+    PATH_TO_LIBRARIES_STORE = File.expand_path('./library/store', __dir__)
+
     def initialize(books = [], authors = [], readers = [], orders = [])
       @authors = authors
       @readers = readers
@@ -35,21 +37,44 @@ module Library
       @orders << Order.new(book, reader, date)
     end
 
-    def form_elem_to_frequency_arr_for(entity)
-      @orders.each_with_object(Hash.new(0)) do |order, h|
+    def form_elem_to_frequency_arr_for(entity, top_amount = orders.length)
+      el_to_freq_hash = orders.each_with_object(Hash.new(0)) do |order, h|
         h[order.send(entity)] += 1
-      end.sort_by { |_k, v| -v }
+      end
+
+      el_to_freq_hash.sort_by { |_ent, freq| -freq }[0...top_amount]
     end
 
-    def top_three_books
-      form_elem_to_frequency_arr_for(:book)[0..2].map(&:first)
+    def top_three_books_with_order_amount
+      form_elem_to_frequency_arr_for(:book, 3)
     end
 
-    def 
+    def top_reader
+      form_elem_to_frequency_arr_for(:reader, 1).first[0]
+    end
+
+    def save_to_file(filename = 'lib.yaml')
+      path = "#{PATH_TO_LIBRARIES_STORE}/#{filename}"
+      File.open(path, 'w') { |f| f.write(to_yaml) }
+    end
+
+    def self.load_from_file(filename = 'lib.yaml')
+      path = "#{PATH_TO_LIBRARIES_STORE}/#{filename}"
+      YAML.load_file(path)
+    end
   end
 end
 
-lib = Library::BookLibrary.new
+lib = Library::BookLibrary.load_from_file
 fake_data_for_lib(lib)
 
-puts lib.top_three_books
+puts "Top reader: #{lib.top_reader}"
+boks_with_order_amount = lib.top_three_books_with_order_amount
+
+puts 'Top books:'
+
+boks_with_order_amount.each_with_index do |arr, i|
+  puts "#{i + 1}.#{arr[0]}. Ordered #{arr[1]} times "
+end
+
+lib.save_to_file
